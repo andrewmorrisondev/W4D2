5fccc7fe45ce22327499ba662c63b83b34634d4f
Jamie Burchfield <burchfield.camilla@gmail.com>
2023-01-31 17:41:44 -0500

    Down to one step

--- class_inheritance.rb
+++ class_inheritance.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Employee
 
   attr_reader :name, :title, :salary, :boss, :employees
@@ -8,7 +10,6 @@ class Employee
     @title = title
     @salary = salary
     @boss = boss
-    @bonus = nil
     boss.employees << self if boss != nil
   end
 
@@ -17,22 +18,38 @@ class Employee
     boss.employees << self
   end
 
-  def bonus=(multiplier)
+  def bonus (multiplier)
     if !self.is_a?(Manager)
-      @bonus = self.salary * multiplier
+     return (self.salary * multiplier)
+    else
+     return (bonus_calculator * multiplier)
     end
   end
+
 
   def bonus_calculator
+    # debugger
     total_salaries = 0
 
-    total_salaries += self.salary if !self.is_a?(Manager)
+    # total_salaries += self.salary if !self.is_a?(Manager)
 
     self.employees.each do |employee|
-      employee.bonus_calculator
+      if employee.is_a?(Manager)
+        # total_salaries += employee.salary + 
+        employee.bonus_calculator
+      else
+        total_salaries += employee.salary
+      end
     end
+    total_salaries
   end
 end
+
+#total salaries = 0
+# self.employees.eachdo |employee|
+#   if employee is a manager?
+#     if yes - run the calcualtor again
+#       if no - total salsairs+= employee.salary
 
 class Manager < Employee
   attr_reader :employees
@@ -45,6 +62,20 @@ class Manager < Employee
   def boss=(boss)
     super
   end
+
+  def bonus_calculator
+    super
+  end
+
+
   
   
 end
+
+ned = Manager.new("Ned", "Founder", 1000000)
+darren = Manager.new("Darren", "TA Manager", 78000, ned)
+david = Employee.new("David", "TA", 10000, darren)
+shawna = Employee.new("Shawna", "TA", 12000, darren)
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
